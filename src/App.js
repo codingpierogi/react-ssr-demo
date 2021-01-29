28ea8f5a43b9b4cfb7fc7e711d800b0e8d2f3493
codingpierogi <jon.pesta@gmail.com>
2021-01-29 10:55:06 +0000

    Initial Pokemon API integration.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import PokemonLandingPage from './PokemonLandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav className="navbar">
+          <a class="navbar-brand" href="#">
+            <img src="" alt="" />
+          </a>
+        </nav>
       </header>
-    </div>
+      <main>
+        <PokemonLandingPage />
+      </main>
+      <footer>
+        Using PokeApi, the RESTful Pokemon API
+      </footer>
+    </>
   );
 }
 
